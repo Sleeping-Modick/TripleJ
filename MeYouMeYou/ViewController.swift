84b4a3391ab73c3be63274b0a1869ddca36d8980
HAN JIWOOK <jiwook.han.dev@gmail.com>
2023-09-18 22:24:57 +0900

    .gitignore 재설정

---
+++ MeYouMeYou/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MeYouMeYou
+//
+//  Copyright (c) 2023 z-wook. All right reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ Pods/Target Support Files/Pods-MeYouMeYou/Pods-MeYouMeYou-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MeYouMeYou : NSObject
+@end
+@implementation PodsDummy_Pods_MeYouMeYou
+@end
