ed7162202f5e3604bce2ce231cb1df68fb476eb1
= <=>
2023-09-20 14:24:52 +0900

    [FEAT] 파일추가

---
+++ MeYouMeYou/DiaryAddPage/Views/DiaryAddView.swift
@@ -0,0 +1,20 @@
+//
+//  DiaryAddView.swift
+//  MeYouMeYou
+//
+//  Created by SeoJunYoung on 2023/09/20.
+//
+
+import UIKit
+
+class DiaryAddView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
